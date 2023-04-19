e36817e045a2cb79a838ddda61ee3aad18e33e29
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-04-18 21:33:46 -0700

    Minor changes before making server

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>So Fetch</h1>
+      <div className="input-form">
+        <h2>Say Something Behind Your Friend's Back</h2>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './components/App';
 // having some issue with styling
 import styles from './styles.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('app'));
