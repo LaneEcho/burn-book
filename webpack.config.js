5536bc3f27daa116ff492fb6d9d84a4f9c7147ff
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-04-18 17:27:10 -0700

    Webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
