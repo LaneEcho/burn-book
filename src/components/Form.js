3f77d38eff8b607c5a5cfe5a325c6caf3273a414
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-04-19 21:42:17 -0700

    WIP end of day 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PostItem from './PostItem';
 
 function FormComponent() {
+  // state hook initialized to empty array
+  const [comment, setComment] = useState([]);
+
+  // function that hopefully adds form input to state - event handler
+
   return (
     <div>
-      <div class="say-something">
+      <div className="say-something">
         <h2>Say Something Behind Your Friend's Back</h2>
         <form action="" id="comment-form">
-          <input
+          <textarea
             type="text"
+            // value={comment}
             id="comment"
             name="comment"
             placeholder="Ms. Norbury is a PUSHER, a SAD OLD DRUG PUSHER."
-          ></input>
+          ></textarea>
           {/* <input type="file"></input> */}
-          <button type="submit">Submit</button>
+          <div id="button-id">
+            <button type="submit">submit</button>
+          </div>
         </form>
       </div>
       <PostItem />
