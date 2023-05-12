5737c177bba7691c3c1730eeba29d8084712de12
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-05-12 14:36:22 -0700

    start of server

---
+++ src/server/server.js
@@ -0,0 +1,22 @@
+// path is a module for working with file paths
+const path = require('path');
+const express = require('express');
+// instance of express library- invoking express
+const app = express();
+const PORT = 3000;
+
+// root endpoint: is '/'
+// get request to '/' --> serve index.html
+// app.get('/', express.static(path.join(__dirname, './index.html')));
+
+// get request to '/getBurns' --> respond with all the burns
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+// error handling
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
