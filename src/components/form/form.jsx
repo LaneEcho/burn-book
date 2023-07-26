9c3b8cf2495433a1b4b09631832dfef5066ede9f
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-07-26 16:41:37 -0700

    submit form partially working

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -3,44 +3,55 @@ import useFetch from '../../hooks/useFetch.jsx';
 import './form.scss';
 
 function FormComponent() {
-  // initialize state
-  const [comment, setComment] = useState(null);
+  // initialize state - comment is empty string
+  const [comment, setComment] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false); // Set loading to false by default
 
-  // add an entry to database
-  const handleSubmit = (event) => {
+  // Event handler for form submission
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const newComment = event.target.elements.comment.value;
-    // check if the value is not empty or whitespace, it will not submit if there is
-    if (newComment.trim() !== '') {
-      setComment([...comment, newComment]);
-      event.target.reset();
+    setLoading(true); // Set loading to true before making the API call
+    try {
+      let res = await fetch('/getBurns', {
+        method: 'POST',
+        body: JSON.stringify({
+          message: comment,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 201) {
+        setComment('');
+        setMessage('Girl on Girl Crime Committed');
+      } else {
+        setMessage('Error occurred in the post request');
+      }
+    } catch (err) {
+      console.log(err);
+      setMessage('Error occurred while fetching data');
     }
+    setLoading(false); // Set loading back to false after the API call is completed
   };
 
-  // function to hopefully handle delete
-  const handleDelete = (index) => {
-    const newComments = [...comment];
-    newComments.splice(index, 1);
-    setComment(newComments);
-    console.log('state: ', comment);
-  };
+  // rest of your code...
+
+  if (loading) {
+    return <div className="loading">Loading...</div>; // Render a loading state while waiting for the Promise to resolve
+  }
 
   return (
     <div className="say-something">
       <h2>Say Something Behind Your Friend's Back</h2>
       <form onSubmit={handleSubmit} id="comment-form">
-        <textarea
+        <input
           type="text"
-          id="comment"
-          name="comment"
+          value={comment}
           placeholder="You let it out, honey. Put it in the book."
-        ></textarea>
-        {/* <input type="file"></input> */}
-        <div id="button-id">
-          <button type="submit" className="submit-button">
-            submit
-          </button>
-        </div>
+          onChange={(event) => setComment(event.target.value)}
+        ></input>
+        <button type="submit" className="submit-button">
+          submit
+        </button>
       </form>
     </div>
   );
