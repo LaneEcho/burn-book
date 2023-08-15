0825788f8478fcdf4e7b63ecce994e53577111c6
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-08-15 16:38:00 -0700

    handleDelete WIP

--- src/components/postItem/postItem.jsx
+++ src/components/postItem/postItem.jsx
@@ -1,16 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './postItem.scss';
 
 function PostItem(props) {
-  // have to make sure we can select by ID
-  // write onDelete handler
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  // function to hopefully handle delete
-  const handleDelete = (index) => {
-    const newComments = [...comment];
-    newComments.splice(index, 1);
-    setComment(newComments);
-    console.log('state: ', comment);
+  // delete function
+  const handleDelete = async (event) => {
+    // confirm that burn will be deleted (true)
+    const result = confirm('Delete this entry?');
+    if (result) {
+      console.log(props.id);
+      //Logic to delete the item
+      setLoading(true);
+
+      try {
+        let res = await fetch('/getBurns', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            id: props.id,
+          }),
+        });
+        let resJson = await res.json(); // do we need this line?
+        if (res.status === 201) {
+          setMessage('Girl on Girl Crime Erased');
+        } else {
+          setMessage('Error occurred in the delete request');
+        }
+      } catch (err) {
+        console.log(err);
+        setMessage(
+          "Fetch didn't happen - Error occurred fetching data in delete request"
+        );
+      }
+
+      setLoading(false); // Set loading back to false after the API call is completed
+    }
+    // else {
+    //   setDisabled(true);
+    //   alert('Please write a comment');
+    // }
   };
 
   return (
@@ -19,7 +51,7 @@ function PostItem(props) {
       <button className="edit-button" onClick={null}>
         edit
       </button>
-      <button className="delete-button" onClick={null}>
+      <button className="delete-button" onClick={handleDelete}>
         delete
       </button>
     </div>
