446ff7f467806dcc664fb2bcd1bbb82f5b9ad0ac
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-12-11 17:16:09 -0800

    reorganization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app.jsx';
-import styles from './App.scss';
 
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
