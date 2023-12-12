23da7da1b6e4790c77c2e417b2db46988942b20f
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-12-11 16:25:10 -0800

    update to react 18

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app.jsx';
-
-// having some issue with styling
 import styles from './App.scss';
 
-ReactDom.render(<App />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
