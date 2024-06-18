e83a0317722dad68bac593a4e7eb2cdc4af37bf7
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-06-17 18:08:49 -0700

    install supabase-js and update databaseModel

--- src/server/databaseModel.js
+++ src/server/databaseModel.js
@@ -1,13 +1,22 @@
-const { Pool } = require('pg');
-const process = require('process');
+// const { Pool } = require('pg');
+// const process = require('process');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PG_URI = process.env.PG_URI;
+// const PG_URI = process.env.PG_URI;
 
-const pool = new Pool({
-  connectionString: PG_URI,
-});
+// const pool = new Pool({
+//   connectionString: PG_URI,
+// });
 
-module.exports = pool;
+// module.exports = pool;
+
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey); // use this instead of db
+
+module.exports = supabase;
