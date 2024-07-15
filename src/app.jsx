43da72280ccc0a7bc574d90be24efbcf1536b70c
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-07-15 15:47:24 -0700

    initial react-query setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Container from './components/container/container.jsx';
+
+// queryClient handles caching, garbage collection, fetching, etc
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Container />
-    </>
+    </QueryClientProvider>
   );
 }
 
