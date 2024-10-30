2f78725d735cf4a631c983e4c7f843ea9f5d1afe
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-29 21:11:31 -0700

    supabase client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
+export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
