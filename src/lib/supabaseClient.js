ce634f909b14b74f77ff3840d064e008262a17f1
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-29 20:54:31 -0700

    Dotenv config

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,6 +14,7 @@ module.exports = {
       title: 'Development',
       template: './src/index.html',
     }),
+    new Dotenv(), // ????
   ],
   module: {
     rules: [
