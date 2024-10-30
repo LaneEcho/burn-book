0bedea455a2bee6a6baf7f9ffca2fb2036e6eef5
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-10-29 20:00:57 -0700

    remove auth0

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,24 +1,14 @@
-import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
-import LoginButton from '../../../lib/auth0/login.js';
-import LogoutButton from '../../../lib/auth0/logout.js';
 
 import './profile.scss';
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
-
-  if (isLoading) {
-    return <div className="loading">Loading ...</div>;
-  }
-
   return (
     <div className="profile">
-      <LoginButton></LoginButton>
-      <LogoutButton></LogoutButton>
-      <h3>{`Welcome to North Shore, ${
+      {/* <h3>{`Welcome to North Shore, ${
         isAuthenticated ? user.name : `friend`
-      }`}</h3>
+      }`}</h3> */}
+      <h3>Welcome to North Shore High</h3>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
 import App from './app.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 
@@ -8,15 +7,7 @@ const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 root.render(
-  <Auth0Provider
-    domain="dev-ibulh8kz68gq6pwf.us.auth0.com"
-    clientId="PzOtmknYafgNAyTXWB34RuqK0QbEPjU6"
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </Auth0Provider>
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
 );
