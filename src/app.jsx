8f1054453f8ab817859d33e875086254cbf55543
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-11-04 19:10:19 -0800

    basic supabase auth-ui

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { supabase } from './lib/supabaseClient.js';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Container from './components/container/container.jsx';
@@ -7,6 +10,53 @@ import Container from './components/container/container.jsx';
 const queryClient = new QueryClient();
 
 function App() {
+  const [session, setSession] = useState(null); // may want to put this in a context
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  // come back to style this
+  if (!session) {
+    return (
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            padding: '1.5rem',
+            borderRadius: '.5rem',
+            backgroundColor: 'white',
+          }}
+        >
+          <Auth
+            supabaseClient={supabase}
+            appearance={{
+              theme: ThemeSupa,
+            }}
+            providers={[]}
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
