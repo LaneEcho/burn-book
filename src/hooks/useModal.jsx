7c0270097d7e47ab96caae4655af1894257b8755
Lane Hamilton <aleyna.hamilton@gmail.com>
2024-12-04 13:42:51 -0800

    useModal hook

---
+++ src/hooks/useModal.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const useModal = () => {
+  const [isShowing, setIsShowing] = useState(false);
+
+  function toggle() {
+    setIsShowing(!isShowing);
+  }
+
+  return [isShowing, toggle];
+};
+
+export default useModal;
