0c3b8bd7b1c2587a446232021342cfe5717041eb
Lane Hamilton <aleyna.hamilton@gmail.com>
2025-08-11 17:35:12 -0700

    ESlint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,37 @@
 import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import globals from 'globals';
 import pluginReact from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    languageOptions: { globals: globals.browser },
-  },
-  pluginReact.configs.flat.recommended,
-  {
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-undef': 'warn',
-      'react/prop-types': 'off',
+export default defineConfig(
+  tseslint.configs.recommended, // recommended
+  // tseslint.configs.strict, // for opinionated rules which may also catch bugs - use later
+  [
+    {
+      files: ['**/*.{js,mjs,cjs,jsx}'],
+      plugins: { js },
+      extends: ['js/recommended'],
     },
-  },
-  {
-    // Note: there should be no other properties in this object
-    ignores: ['dist/*', 'webpack.config.js'],
-  },
-  eslintConfigPrettier,
-]);
+    {
+      files: ['**/*.{js,mjs,cjs,jsx}'],
+      languageOptions: { globals: globals.browser },
+    },
+    pluginReact.configs.flat.recommended,
+    {
+      rules: {
+        'no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-require-imports': 'warn',
+        'no-undef': 'warn',
+        'react/prop-types': 'off',
+      },
+    },
+    {
+      // Note: there should be no other properties in this object
+      ignores: ['dist/*', 'webpack.config.js'],
+    },
+    eslintConfigPrettier,
+  ]
+);
